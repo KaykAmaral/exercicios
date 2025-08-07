934690374509a5bed5bb81a6886f101e488d6eac
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-06 21:11:27 -0300

    Generics
    Programa bem simples para fixar o conceito principal de generic. Estou apanhando bastante para aprender isso.

---
+++ teste_1/Application.java
@@ -0,0 +1,34 @@
+package exercicios.teste_1;
+
+import exercicios.teste_1.Services.PrintService;
+
+import java.util.Scanner;
+public class Application {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        PrintService<String> names = new PrintService<>();
+        PrintService<Integer> ages = new PrintService<>();
+
+        System.out.print("How many data you'll text?   ");
+        int x = sc.nextInt();
+
+        for (int i = 0; i < x; i++) {
+            System.out.print("Name:   ");
+            names.addItem(sc.next());
+            System.out.print("Age:    ");
+            ages.addItem(sc.nextInt());
+        }
+
+        System.out.println("1st name & age:");
+        System.out.println("Name:  " + names.first());
+        System.out.println("Age:   " + ages.first());
+
+        sc.close();
+
+    }
+
+}
---
+++ teste_1/Services/PrintService.java
@@ -0,0 +1,17 @@
+package exercicios.teste_1.Services;
+
+import java.util.ArrayList;
+
+public class PrintService<T> {
+    // The 'T' is a generic type, it can be String, Double, int, Integer... Whatever...
+
+    private ArrayList<T> list = new ArrayList<>();
+
+    public void addItem(T value) { list.add(value); }
+
+    public T first() {
+        if (list.isEmpty()) { throw new IllegalStateException("[ERROR] - The list is Empty"); }
+        return list.getFirst();
+    }
+
+}
