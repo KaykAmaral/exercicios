55366a3bc75a1a6c7fded46b6ec23203dd144221
KaykAmaral <kayaquiurbano@gmail.com>
2025-08-11 21:21:47 -0300

    Testing GitKraken in Ubuntu

--- teste_5/Application.java
+++ teste_5/Application.java
@@ -1,62 +1,62 @@
 package exercicios.teste_5;
 
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.ArrayList;
+import exercicios.teste_5.entities.Client;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Client c1 = new Client("Kayk", "123-45-6789");
+        Client c2 = new Client("Martha", "456-88-7511");
+        Client c3 = new Client("Kayk", "123-45-6789");
 
-        ArrayList<Integer> integers = new ArrayList<>();
-        ArrayList<Double> doubles = new ArrayList<>();
-        ArrayList<Object> copyIntegers = new ArrayList<>();
-        ArrayList<Object> copyDoubles = new ArrayList<>();
+        String s1 = "test";
+        String s2 = "test";
+        String s3 = new String("test");
+        String s4 = new String("test");
 
-        System.out.println("Text three int numbers:");
-        for (int i = 1; i <= 3; i++) {
-            System.out.print(i + ": ");
-            Integer x = sc.nextInt();
-            integers.add(x);
-        }
-        System.out.println("Text three double numbers:");
-        for (int i = 1; i <= 3; i++) {
-            System.out.print(i + ": ");
-            Double x = sc.nextDouble();
-            doubles.add(x);
-        }
+        // Tests with '==':
+        System.out.println("TESTS WITH '==':");
+        System.out.println(s1 == s2); // True
+        System.out.println(s2 == s3); // False
+        System.out.println(s3 == s4); // False
+        System.out.println(c1 == c2); // False
+        System.out.println(c1 == c3); // False
 
+        /*
+            Why'd the c1 aren't equal to c3?
+            Cause using '==' you're catting the  Object's address code and comparing this, not the content inside
+            The same thing about s3 and s4
+            So... s1 is equals s2, because they've a special treatment and they aren't considered an object
+         */
 
-        System.out.print("\nIntegers Copy: ");
-        copy(integers, copyIntegers);
-        printList(copyIntegers);
-        System.out.print("Double Copy:   ");
-        copy(doubles, copyDoubles);
-        printList(copyDoubles);
+        // Test with '.equals()'
+        System.out.println("\nTESTS WITH '.equals()'");
+        System.out.println(s1.equals(s2)); // True
+        System.out.println(s2.equals(s3)); // True
+        System.out.println(s3.equals(s4)); // True
+        System.out.println(c1.equals(s2)); // False
+        System.out.println(c1.equals(c3)); // True
 
-    }
+        /*
+            The equals method compare the contend inside the objects
+         */
 
-    public static void copy(ArrayList<? extends Number> source, ArrayList<? super Number> destiny) {
-        /* This can replace to:
-        * for (Number number : source) {
-        *   destiny.add(number);
-        * }
-        */
-        destiny.addAll(source);
-    }
+        // Tests with hashcode:
+        System.out.println("\nTESTS WITH 'hashcode':");
 
-    public static void printList(ArrayList<?> list) {
-        if (list.isEmpty()) {
-            System.out.println("[ERROR] - List can't be empty!");
-        } else {
-            System.out.print("[   ");
-            for (Object o : list) {
-                System.out.print(o + "   ");
-            }
-            System.out.println("]\n");
-        }
+        System.out.println(c1.hashCode()); // 1668029586
+        System.out.println(c2.hashCode()); // -1733444430
+        System.out.println(c3.hashCode()); // 1668029586
+        System.out.println(s1.hashCode()); // 3556498
+        System.out.println(s2.hashCode()); // 3556498
+        System.out.println(s3.hashCode()); // 3556498
+        System.out.println(s4.hashCode()); // 3556498
+
+        /*
+            The hashcode are generated using the Object's content inside itself
+         */
+
     }
 
 }
