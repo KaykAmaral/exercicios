6eaa3c6de43f72071f5a082bcf6dc87682e6bfcf
Kayk Eduardo Stefano Amaral <kayaquiurbano@gmail.com>
2025-10-07 16:15:12 -0300

    Function<T, R>

---
+++ teste_10/Application.java
@@ -0,0 +1,33 @@
+package exercicios.teste_10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 80.90));
+
+        /*
+        * Using .stream() we transform the list to stream
+        * .collect(Collectors.toList()) transform a stream to List
+        *  We also can use just .toList()
+        * */
+
+        List<String> names = list.stream().map( p -> p.getName().toUpperCase() ).toList();
+
+        names.forEach(System.out::println);
+
+    }
+
+}
